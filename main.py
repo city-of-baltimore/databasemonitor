84b48423b763dc579262505b3b8bc1c1df5a19a4
Brian Seel <Brian.Seel@baltimorecity.gov>
2021-01-28 10:30:48 -0500

    updating config

--- main.py
+++ main.py
@@ -5,59 +5,76 @@ from typing import List
 from dbmonitor import ConfigType, DBMonitor
 
 config: List[ConfigType] = [
+    # dataflow runs daily at 12am +4 hours
     {
         'table_name': 'acrs_crash',
         'date_col': 'ACRSREPORTTIMESTAMP',
         'email': 'brian.seel@baltimorecity.gov',
-        'notification_mins': 1440  # 24 hours
+        'notification_mins': 2880  # 48 hours
     },
+
+    # dataflow runs daily at 12am +4 hours
     {
         'table_name': 'atves_amber_time_rejects',
         'date_col': 'violation_date',
         'email': 'brian.seel@baltimorecity.gov',
-        'notification_mins': 1440  # 24 hours
+        'notification_mins': 2880  # 48 hours
     },
+
+    # dataflow runs from 6:30am-12:30am every 5 minutes
     {
         'table_name': 'ccc_bus_runtimes',
         'date_col': 'starttime',
         'email': 'brian.seel@baltimorecity.gov',
         'notification_mins': 720  # 12 hours
     },
+
+    # dataflow runs from 6:30am-12:30am every 1 minute
     {
         'table_name': 'ccc_arrival_times',
         'date_col': 'date',
         'email': 'brian.seel@baltimorecity.gov',
         'notification_mins': 15
     },
+
+    # dataflow runs daily at 12am +4 hours
     {
         'table_name': 'atves_approval_by_review_date_details',
         'date_col': 'review_datetime',
         'email': 'brian.seel@baltimorecity.gov',
         'notification_mins': 2880  # 48 hours
     },
+
+    # dataflow runs daily at 12am +4 hours
     {
         'table_name': 'atves_by_location',
         'date_col': 'date',
         'email': 'brian.seel@baltimorecity.gov',
-        'notification_mins': 1440  # 24 hours
+        'notification_mins': 2880  # 48 hours
     },
+
+    # dataflow runs daily at 12am +4 hours
     {
         'table_name': 'atves_traffic_counts',
         'date_col': 'date',
         'email': 'brian.seel@baltimorecity.gov',
-        'notification_mins': 1440  # 24 hours
+        'notification_mins': 2880  # 48 hours
     },
+
+    # dataflow runs daily at 12am +4 hours
     {
         'table_name': 'ticketstat',
         'date_col': 'Export_Date',
         'email': 'brian.seel@baltimorecity.gov',
-        'notification_mins': 1440  # 24 hours
+        'notification_mins': 2880  # 48 hours
     },
+
+    # dataflow runs daily at 12am +4 hours
     {
         'table_name': 'towstat_agebydate',
         'date_col': 'date',
         'email': 'brian.seel@baltimorecity.gov',
-        'notification_mins': 1440  # 24 hours
+        'notification_mins': 2880  # 48 hours
     },
 ]
 
