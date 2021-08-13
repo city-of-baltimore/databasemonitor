7d83e6155a8c78f64251cc84a17dcbf5e6dfe44d
Brian Seel <Brian.Seel@baltimorecity.gov>
2021-08-13 16:55:10 -0400

    Moved argument parsing to the module level and removed the driver code. Added some logic to handle missing databases. Added some improved testing as well.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 """Pytest fixtures"""
 from datetime import datetime, timedelta
-import tempfile
 import pytest
 
 from sqlalchemy import Column, create_engine  # type: ignore
@@ -15,6 +14,7 @@ Base: DeclarativeMeta = declarative_base()
 class TestTableGood(Base):  # pylint:disable=too-few-public-methods
     """Table used for the successful test case with recent activity"""
     __tablename__ = 'testtablegood'
+
     ID = Column(Integer, primary_key=True)
     DATETIME = Column(DateTime)
 
@@ -22,6 +22,7 @@ class TestTableGood(Base):  # pylint:disable=too-few-public-methods
 class TestTableFail(Base):  # pylint:disable=too-few-public-methods
     """Table used for the unsuccessful test case with no recent activity"""
     __tablename__ = 'testtablefail'
+
     ID = Column(Integer, primary_key=True)
     DATETIME = Column(DateTime)
 
@@ -29,22 +30,22 @@ class TestTableFail(Base):  # pylint:disable=too-few-public-methods
 def pytest_addoption(parser):
     """Adds command line options"""
     # this account is only used for testing, so hard coding creds is fine
-    parser.addoption('--email_username', default='bcdotnotifications@gmail.com', action='store')
-    parser.addoption('--email_password', default='eV^5d97%', action='store')
-    parser.addoption('--pop_server', default='pop.gmail.com', action='store')
-    parser.addoption('--smtp_server', default='smtp.gmail.com', action='store')
+    parser.addoption('--email_username', action='store')
+    parser.addoption('--email_password', action='store')
+    parser.addoption('--pop_server', action='store')
+    parser.addoption('--smtp_server', action='store')
 
 
 @pytest.fixture(name='email_username')
 def email_username_fixture(request):
     """email username fixture (smtp and pop)"""
-    return request.config.getoption('email_username')
+    return request.config.getoption('email_username', skip=True)
 
 
 @pytest.fixture(name='email_password')
 def email_password_fixture(request):
     """email password fixture (smtp and pop)"""
-    return request.config.getoption('email_password')
+    return request.config.getoption('email_password', skip=True)
 
 
 @pytest.fixture(name='smtp_server')
@@ -66,24 +67,23 @@ def notification_window_fixture():
 
 
 @pytest.fixture()
-def test_db(notification_window):
+def test_db(notification_window, tmp_path_factory):
     """Fixture that sets up test database"""
-    with tempfile.TemporaryFile() as tmp_file:
-        conn_str = 'sqlite:///{}.db'.format(tmp_file.name)
-        engine = create_engine(conn_str, echo=True, future=True)
+    conn_str = 'sqlite:///{}.db'.format(str(tmp_path_factory.mktemp('data') / 'database.db'))
+    engine = create_engine(conn_str, echo=True, future=True)
 
-        with engine.begin() as connection:
-            Base.metadata.create_all(connection)
+    with engine.begin() as connection:
+        Base.metadata.create_all(connection)
 
-        with Session(bind=engine, future=True) as session:
-            for i in range(10):
-                # add data for passing test
-                session.add(TestTableGood(DATETIME=datetime.now() - timedelta(minutes=240 + (60 * i))))
+    with Session(bind=engine, future=True) as session:
+        for i in range(10):
+            # add data for passing test
+            session.add(TestTableGood(DATETIME=datetime.now() - timedelta(minutes=240 + (60 * i))))
 
-            for i in range(10):
-                # add data for failing test
-                session.add(TestTableFail(DATETIME=datetime.now() - timedelta(minutes=notification_window + (60 * i))))
+        for i in range(10):
+            # add data for failing test
+            session.add(TestTableFail(DATETIME=datetime.now() - timedelta(minutes=notification_window + (60 * i))))
 
-            session.commit()
+        session.commit()
 
-        yield conn_str
+    yield conn_str
--- tests/test_dbmonitor.py
+++ tests/test_dbmonitor.py
@@ -1,44 +1,70 @@
 """Test suite for dbmonitor.py"""
+import os
 import poplib
 import uuid
 from email import parser
+from unittest import mock
 
 import pytest
+from loguru import logger
 
 from dbmonitor import DBMonitor
+from dbmonitor.dbmonitor import parse_args, setup_logging
 
 
-def test_check_db_success(test_db, email_username, notification_window):
+def test_check_db_success(test_db, notification_window):
     """Test case where a notification should not be generated"""
     table_config = [
         {'table_name': 'testtablegood',
          'date_col': 'DATETIME',
          'notification_mins': notification_window,
-         'email': email_username
+         'email': 'dummyusername'
          }
     ]
 
-    # this should not send an email, and will fail with these creds if it tries
-    cls = DBMonitor(conn_str=test_db, email_user='xxx', email_pass='xxx')
-    cls.check(configs=table_config)
+    with mock.patch('dbmonitor.DBMonitor.send_notification') as send_notification_mock:
+        # this should not send an email, and will fail with these creds if it tries
+        cls = DBMonitor(conn_str=test_db, email_user='xxx', email_pass='xxx')
+        cls.check(configs=table_config)
+        send_notification_mock.assert_not_called()
 
 
-def test_check_db_failure(test_db, email_username, email_password, notification_window):
+def test_check_db_failure(test_db, notification_window):
     """Test case where a notification should be generated"""
     table_config = [
         {'table_name': 'testtablefail',
          'date_col': 'DATETIME',
          'notification_mins': notification_window,
-         'email': email_username
+         'email': 'dummyusername'
          }
     ]
-    cls = DBMonitor(conn_str=test_db, email_user=email_username, email_pass=email_password)
-    cls.check(configs=table_config)
+
+    with mock.patch('dbmonitor.DBMonitor.send_notification') as send_notification_mock:
+        cls = DBMonitor(conn_str=test_db, email_user='dummyusername', email_pass='dummypassword')
+        cls.check(configs=table_config)
+        send_notification_mock.assert_called_once()
+
+
+def test_check_db_dne(test_db, notification_window):
+    """Test case where the table doesn't exist"""
+    table_config = [
+        {'table_name': 'table_dne',
+         'date_col': 'DATETIME',
+         'notification_mins': notification_window,
+         'email': 'dummyusername'
+         }
+    ]
+
+    with mock.patch('dbmonitor.DBMonitor.send_notification') as send_notification_mock:
+        cls = DBMonitor(conn_str=test_db, email_user='dummyusername', email_pass='dummypassword')
+        cls.check(configs=table_config)
+        send_notification_mock.assert_called_once()
 
 
 def send_notification(email_username, email_password, smtp_server, pop_server,  # pylint:disable=too-many-arguments
                       notification_window, secure: bool):
     """Helper for test_send_notification_secure and test_send_notification_unsecure"""
+
     cls = DBMonitor('sqlite://', email_username, email_password, smtp_server)
 
     # send a unique id so we make sure we get the right email
@@ -77,3 +103,68 @@ def test_check_required_arguments(test_db):
     """Tests that an exception is thrown when missing arguments are missing"""
     with pytest.raises(ValueError):
         DBMonitor(conn_str=test_db, email_user=None, email_pass=None, smtp_server=None)
+
+
+def test_setup_logging(tmp_path_factory):
+    """Tests setup_logging"""
+    def verify_log_size(log_path, size):
+        logs = log_path.glob('logs/file-*.log')
+        for log in logs:
+            assert os.path.getsize(log) >= size * .9
+            assert os.path.getsize(log) <= size * 1.1
+
+    # test to make sure warning logs, but not info or debug
+    path = tmp_path_factory.getbasetemp() / 'warn'
+    setup_logging(False, False, path)
+    logger.warning('Test log')
+    logger.info('Test log')
+    logger.debug('Test log)')
+    verify_log_size(path, 700)
+
+    # test to make sure warning and info logs, but not debug
+    path = tmp_path_factory.getbasetemp() / 'info'
+    setup_logging(False, True, path)
+    logger.warning('Test log')
+    logger.info('Test log')
+    logger.debug('Test log)')
+    verify_log_size(path, 1400)
+
+    # test to make sure everything up to debug logs
+    path = tmp_path_factory.getbasetemp() / 'debug'
+    setup_logging(True, True, path)
+    logger.warning('Test log')
+    logger.info('Test log')
+    logger.debug('Test log)')
+    verify_log_size(path, 2100)
+
+
+def test_parse_args():
+    """Tests parse_args"""
+    conn_str = 'conn_str'
+    email_addr = 'email_addr'
+    email_password = 'email_password'
+    smtp_server = 'smtp_server'
+    config = 'config.py'
+
+    args = parse_args(['-v', '-vv', '-s', '--conn_str', conn_str, '--email_address', email_addr, '--smtp_server',
+                       smtp_server, '--config', config])
+
+    assert args.debug
+    assert args.verbose
+    assert args.conn_str == conn_str
+    assert args.email_address == email_addr
+    assert args.smtp_server == smtp_server
+    assert args.secure
+    assert args.config == config
+
+    args = parse_args(['--conn_str', conn_str, '--email_address', email_addr, '--email_password', email_password,
+                       '--smtp_server', smtp_server, '--config', config])
+
+    assert not args.debug
+    assert not args.verbose
+    assert args.conn_str == conn_str
+    assert args.email_address == email_addr
+    assert args.smtp_server == smtp_server
+    assert not args.secure
+    assert args.config == config
+    assert args.email_password == email_password
